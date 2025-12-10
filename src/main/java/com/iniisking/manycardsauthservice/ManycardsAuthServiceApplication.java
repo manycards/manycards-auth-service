f44af4e968ff5ed92b92c4d78dc560c60bbb1dee
IniOluwa Longe <inilonge@gmail.com>
2025-12-10 18:42:15 +0100

    Initial commit: Spring Boot auth service with PostgreSQL configuration

---
+++ src/main/java/com/iniisking/manycardsauthservice/ManycardsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iniisking.manycardsauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManycardsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManycardsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iniisking/manycardsauthservice/ManycardsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniisking.manycardsauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManycardsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
